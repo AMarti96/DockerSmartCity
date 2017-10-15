e34d8dee3ba2192b63cdc3c61372154653dbec0e
Adrià.Martí <adria.marti@aaaida.com>
2017-10-15 05:38:38 +0200

    mas modelos

--- airquality_model.js
+++ airquality_model.js
@@ -7,22 +7,46 @@ var airquality = {
     "name":"6772696769746f",
     "token":"f37a27ae85763ebe4276455d873ec12d206a96ae1c818f2717376a3cd3165e7a",
     "variables":[
-            {
-                "sensor":"CO2_Meter1",
-                "location":"41.3925 2.1450999999999567"
-            },
-            {
-                "sensor":"CO2_Meter2",
-                "location":"41.3913 2.1801000000000386"
-            },
-            {
-                "sensor":"CO2_Meter3",
-                "location":"41.3888 2.158990000000017"
-            },
-            {
-                "sensor":"CO2_Meter4",
-                "location":"41.38272233367812 2.1769505739212036"
-            }
+        {
+            "sensor":"CO2_Meter1",
+            "location":"41.3925 2.1450999999999567"
+        },
+        {
+            "sensor":"CO2_Meter2",
+            "location":"41.3913 2.1801000000000386"
+        },
+        {
+            "sensor":"CO2_Meter3",
+            "location":"41.3888 2.158990000000017"
+        },
+        {
+            "sensor":"CO2_Meter4",
+            "location":"41.38272233367812 2.1769505739212036"
+        },
+        {
+            "sensor":"CO2_Meter5",
+            "location":"41.37295699848027 2.163362503051758"
+        },
+        {
+            "sensor":"CO2_Meter6",
+            "location":"41.384028022114954 2.1448230743408203"
+        },
+        {
+            "sensor":"CO2_Meter7",
+            "location":"41.41003976060891 2.1532344818115234"
+        },
+        {
+            "sensor":"CO2_Meter8",
+            "location":"41.40765793016418 2.179584503173828"
+        },
+        {
+            "sensor":"CO2_Meter9",
+            "location":"41.39719615711401 2.200913429260254"
+        },
+        {
+            "sensor":"CO2_Meter10",
+            "location":"41.38734445555327 2.1234512329101562"
+        }
     ]
 }
 module.exports = airquality;
--- parking_model.js
+++ parking_model.js
@@ -4,26 +4,46 @@ var parking = {
     "name":"6d61736d69",
     "token":"b09af6daff593bfc2ddc612947e8e1e7baa8db30d0a7da6cda9964de2e183d47",
     "variables":[
-    {
-        "sensor":"Parking_Meter1",
-        "location":"41.38665542419419 2.166163936126395"
-    },
-    {
-        "sensor":"Parking_Meter2",
-        "location":"41.40350561240383 2.174537390274054"
-    },
-    {
-        "sensor":"Parking_Meter3",
-        "location":"41.392759856096234 2.1448724254150875"
-    },
-    {
-        "sensor":"Parking_Meter4",
-        "location":"41.394920095556266 2.175614833831787"
-    },
-    {
-        "sensor":"Parking_Meter5",
-        "location":"41.38893499432572 2.120406655885745"
-    }
+        {
+            "sensor":"Parking_Meter1",
+            "location":"41.38665542419419 2.166163936126395"
+        },
+        {
+            "sensor":"Parking_Meter2",
+            "location":"41.40350561240383 2.174537390274054"
+        },
+        {
+            "sensor":"Parking_Meter3",
+            "location":"41.392759856096234 2.1448724254150875"
+        },
+        {
+            "sensor":"Parking_Meter4",
+            "location":"41.394920095556266 2.175614833831787"
+        },
+        {
+            "sensor":"Parking_Meter5",
+            "location":"41.38893499432572 2.120406655885745"
+        },
+        {
+            "sensor":"Parking_Meter6",
+            "location":"41.3805826195271 2.1540069580078125"
+        },
+        {
+            "sensor":"Parking_Meter7",
+            "location":"41.38560576303636 2.1494579315185547"
+        },
+        {
+            "sensor":"Parking_Meter8",
+            "location":"41.38686149061934 2.1341371536254883"
+        },
+        {
+            "sensor":"Parking_Meter9",
+            "location":"41.37713703341386 2.1582984924316406"
+        },
+        {
+            "sensor":"Parking_Meter10",
+            "location":"41.4026042976081 2.1607446670532227"
+        }
     ]
 }
 module.exports = parking;
